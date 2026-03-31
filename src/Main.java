3193526c3df84d82be3f31ffc25aa0f6a1ffc5da
김나예 <nayekk1207@gmail.com>
2026-03-31 20:48:54 +0900

    feat: 계산기 기능

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("=== 자바 계산기 프로그램 ===");
+        System.out.println("더하기: " + (a + b));
+        System.out.println("빼기: " + (a - b));
+        System.out.println("곱하기: " + (a * b));
+        System.out.println("나누기: " + (a / b));
+    }
+}
